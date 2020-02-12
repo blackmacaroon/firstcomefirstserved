7cce34eb80b5176c603baddf9773a1cee9729974
Kayla <6lackmacaroon@gmail.com>
2020-02-12 13:42:36 -0700

    comment code to understand process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // base case, there are no orders left, return true
+// not necessarily consecutive order numbers
 
 // break into sub problems: 
 //      check the first served (should be the first ordered)
@@ -7,17 +8,21 @@
 //      recursively check servedorder[0] with the [0] index, if it doesn't match return false
 
 function fcfs(takeOutOrders, dineInOrders, servedOrders) {
+    // base case
     if (servedOrders === 0) {
         return true
     }
-
+    // compare the first order from served, if it's first in takeout
     if(takeOutOrders[0] === servedOrders[0]){
+        // the order is accounted for and it's removed from both lists
         return fcfs(takeOutOrders.slice(1), dineInOrders, servedOrders.slice(1))
     }
-
+    // if not, compare the first order from served, if it's the first in takeout
     else if(dineInOrders[0] === servedOrders[0]){
+        // the order is accounted for and it's removed from both lists
         return fcfs(dineInOrders.slice(1), takeOutOrders, servedOrders.slice(1))
     }
+    // if neither are true, it was not first-come-first served
     else {
         return false
     }
